63680427cb5944ca2f53f1366d8c3b656f24c9ed
Nathan Pinaud <nathan3007yt@gmail.com>
2025-01-16 14:25:57 +0100

    test

--- server/index.js
+++ server/index.js
@@ -18,47 +18,13 @@ app.use((req, res, next) => {
 // Création du graphe
 const g = rdf.graph();
 
+const file = "./ontology";
+
+const fs = require("fs");
+
+const ontologie = fs.readFileSync(file, "utf-8");
+
 // Chargement de l'ontologie (en français)
-const ontologie = `
-  @prefix : <http://example.org/ontologies/voiture#> .
-  @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-  @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-
-  :Voiture rdf:type rdfs:Class .
-  :Marque rdf:type rdfs:Class .
-  :TypeCarburant rdf:type rdfs:Class .
-  :Usage rdf:type rdfs:Class .
-  :Style rdf:type rdfs:Class .
- 
-  :produitePar rdf:type rdf:Property ; rdfs:domain :Voiture ; rdfs:range :Marque .
-  :aTypeCarburant rdf:type rdf:Property ; rdfs:domain :Voiture ; rdfs:range :TypeCarburant .
-  :estConçuePour rdf:type rdf:Property ; rdfs:domain :Voiture ; rdfs:range :Usage .
-  :aStyle rdf:type rdf:Property ; rdfs:domain :Voiture ; rdfs:range :Style .
-
-  :StyleSport rdf:type :Style .
-  :LigneDroite rdf:type :Style .
-  :Urbain rdf:type :Usage .
-
-  :Tesla rdf:type :Marque .
-  :Toyota rdf:type :Marque .
-  :Volkswagen rdf:type :Marque .
-
-  :ModelS rdf:type :Voiture ;
-    :produitePar :Tesla ;
-    :aTypeCarburant :Électrique ;
-    :estConçuePour :Urbain ;
-    :aStyle :StyleSport .
-
-  :Supra rdf:type :Voiture ;
-    :produitePar :Toyota ;
-    :aTypeCarburant :Essence ;
-    :aStyle :LigneDroite .
-
-  :GolfGTI rdf:type :Voiture ;
-    :produitePar :Volkswagen ;
-    :aTypeCarburant :Essence ;
-    :aStyle :LigneDroite .
-`;
 
 // Charger les données dans le graphe
 rdf.parse(
@@ -88,56 +54,135 @@ const executeQuery = (query) => {
   });
 };
 
-// Endpoint pour rechercher des voitures par style
-app.get("/voitures/style", (req, res) => {
-  const style = req.query.style;
+app.get("/style", async (req, res) => {
   const query = `
-    PREFIX : <http://example.org/ontologies/voiture#>
-    SELECT ?voiture ?marque
-    WHERE {
-      ?voiture :aStyle :${style} ;
-               :produitePar ?marque .
-    }
-  `;
+        PREFIX : <http://example.org/ontologies/voiture#>
+        SELECT ?style
+        WHERE {
+        ?style rdf:type :Style .
+        }
+    `;
   try {
     const results = rdf.SPARQLToQuery(query, false, g);
     const formattedResults = [];
     g.query(results, (row) => {
-      formattedResults.push({
-        voiture: row.voiture.value.split("#")[1],
-        marque: row.marque.value.split("#")[1],
-      });
+      console.log(row);
+      formattedResults.push(row.style.value.split("#")[1]);
     });
-    res.json({ results: formattedResults });
+    setTimeout(() => {
+      res.json({ results: formattedResults });
+    }, 2000);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
-// Endpoint pour rechercher des voitures par type de carburant
-app.get("/voitures/carburant", async (req, res) => {
-  const carburant = req.query.carburant;
+app.get("/carburant", async (req, res) => {
   const query = `
+            PREFIX : <http://example.org/ontologies/voiture#>
+            SELECT ?carburant
+            WHERE {
+            ?carburant rdf:type :TypeCarburant .
+            }
+        `;
+  try {
+    const results = rdf.SPARQLToQuery(query, false, g);
+    const formattedResults = [];
+    g.query(results, (row) => {
+      formattedResults.push(row.carburant.value.split("#")[1]);
+    });
+    setTimeout(() => {
+      res.json({ results: formattedResults });
+    }, 2000);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/marques", async (req, res) => {
+  const query = `
+                PREFIX : <http://example.org/ontologies/voiture#>
+                SELECT ?marque
+                WHERE {
+                ?marque rdf:type :Marque .
+                }
+            `;
+  try {
+    const results = rdf.SPARQLToQuery(query, false, g);
+    const formattedResults = [];
+    g.query(results, (row) => {
+      formattedResults.push(row.marque.value.split("#")[1]);
+    });
+    setTimeout(() => {
+      res.json({ results: formattedResults });
+    }, 2000);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Cylindre
+app.get("/cylindre", async (req, res) => {
+  const query = `
+                    PREFIX : <http://example.org/ontologies/voiture#>
+                    SELECT ?cylindre
+                    WHERE {
+                    ?cylindre rdf:type :Cylindre .
+                    }
+                `;
+  try {
+    const results = rdf.SPARQLToQuery(query, false, g);
+    const formattedResults = [];
+    g.query(results, (row) => {
+      formattedResults.push(row.cylindre.value.split("#")[1]);
+    });
+    setTimeout(() => {
+      res.json({ results: formattedResults });
+    }, 2000);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Consommation
+
+app.get("/consommation", async (req, res) => {
+  const query = `
+                            PREFIX : <http://example.org/ontologies/voiture#>
+                            SELECT ?consommation
+                            WHERE {
+                            ?consommation rdf:type :Consommation .
+                            }
+                        `;
+  try {
+    const results = rdf.SPARQLToQuery(query, false, g);
+    const formattedResults = [];
+    g.query(results, (row) => {
+      formattedResults.push(row.consommation.value.split("#")[1]);
+    });
+    setTimeout(() => {
+      res.json({ results: formattedResults });
+    }, 2000);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// endpoint pour rechercher des voitures par plusieurs critères
+app.get("/voitures", async (req, res) => {
+  const { style, carburant } = req.query;
+  let query = `
     PREFIX : <http://example.org/ontologies/voiture#>
     SELECT ?voiture ?marque
     WHERE {
-      ?voiture :aTypeCarburant :${carburant} ;
+      ?voiture :aStyle :${style} ;
+               :aTypeCarburant :${carburant} ;
                :produitePar ?marque .
     }
   `;
   try {
-    const results = rdf.SPARQLToQuery(query, false, g);
-    let formattedResults = [];
-    g.query(results, (row) => {
-      formattedResults.push({
-        voiture: row["?voiture"].value.split("#")[1],
-        marque: row["?marque"].value.split("#")[1],
-      });
-    });
-
-    setTimeout(() => {
-      res.json({ results: formattedResults });
-    }, 2000);
+    const results = await executeQuery(query);
+    res.json({ results });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
